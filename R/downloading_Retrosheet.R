659ab30da192e3d1b2bb6cbc157dea4fac18aa89
Jim Albert <albertcb1@gmail.com>
2021-04-28 19:33:00 -0400

    add files

---
+++ R/downloading_Retrosheet.R
@@ -0,0 +1,13 @@
+# set up folder named download.folder with two 
+# subfolders named zipped and unzipped
+
+# navigate to download.folder
+
+# if you want 2018 data
+# parse.retrosheet2.pbp(2018)
+
+# navigate to unzipped folder
+# to add header and add runs values
+# d2018 <- compute.runs.expectancy(2018)
+
+
